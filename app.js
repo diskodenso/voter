545c2c01d1863b6bda2f9d28622f3ec5a6658f20
Denso666 <Denisolf@mx.de>
2022-10-18 22:17:27 +0200

    add data option

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
 // Konfiguriere die Root-Component
 const app = Vue.createApp({
-    // anschließend werden die Optionen festgelegt - option api
-
+  // anschließend werden die Optionen festgelegt - option api
+  // DATA erwartet eine funktion
+  data: function () {
+    // muss objekt zurückliefern - bleibt reactiv
+    return {
+      // Objekte in Javascript haben immer einen schlüssel und einen wert
+      submissions: submissions, // seed.js
+    };
+  },
 });
 
-
-
 // Liefert eine Instanz zur Root-Component zurück
-const vm = app.mount("#app")
+const vm = app.mount("#app");
