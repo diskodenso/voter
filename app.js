f103dabb8ac487d9cbb2eb48464b58c392267834
denis <denis.olf@itmx.de>
2022-10-21 13:17:57 +0200

    add childcomponent and pass props

--- app.js
+++ app.js
@@ -44,14 +44,14 @@ const app = Vue.createApp({
     //   console.log(this);
     // }
     //Normale Funktion verwenden
-    upvote(submissionId) {
-      // console.log(this)
-      const submission = this.submissions.find(
-        (submission) => submission.id === submissionId
-      );
-      submission.votes++;
-      // console.log(event);
-    },
+    // upvote(submissionId) {
+    //   // console.log(this)
+    //   const submission = this.submissions.find(
+    //     (submission) => submission.id === submissionId
+    //   );
+    //   submission.votes++;
+    // console.log(event);
+    // },
     // logConsole(text) {
     //   console.log(text);
     // },
@@ -62,34 +62,96 @@ const app = Vue.createApp({
     //     return totalVotes + submission.votes;
     //   }, 0);
     // }
-  },
-  watch: {
-    // funktioniert nur bei primitiven datentypen und ist eine abkürzung zur unteren schreibweise
-    // submissions(newValue, oldValue) {
-    //   console.log(newValue);
-    //   console.log(oldValue);
-    //},
-    // Bei dieser schreibweise können wir weitere Optionen hinzufügen
-    // submissions: {
-    //   // handler ist option 1
-    //   handler(newValue, oldValue) {
-    //     this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
-    //       return totalVotes + submission.votes;
-    //     }, 0);
-    //     console.log(newValue);
-    //     console.log(oldValue);
-    //   },
-    //   // deep ist option 2
-    //   deep: true, // auch daten innerhalb des arrays oder objects sollen beobachtet werden
-    //   // immediate 3. Option
-    //   immediate: true,// option das watcher sofort ausgeführt wird
     // },
-    // totalVotes(newValue, oldValue) {
-    //   console.log(newValue);
-    //   console.log(oldValue);
-    // },
+    watch: {
+      // funktioniert nur bei primitiven datentypen und ist eine abkürzung zur unteren schreibweise
+      // submissions(newValue, oldValue) {
+      //   console.log(newValue);
+      //   console.log(oldValue);
+      //},
+      // Bei dieser schreibweise können wir weitere Optionen hinzufügen
+      // submissions: {
+      //   // handler ist option 1
+      //   handler(newValue, oldValue) {
+      //     this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
+      //       return totalVotes + submission.votes;
+      //     }, 0);
+      //     console.log(newValue);
+      //     console.log(oldValue);
+      //   },
+      //   // deep ist option 2
+      //   deep: true, // auch daten innerhalb des arrays oder objects sollen beobachtet werden
+      //   // immediate 3. Option
+      //   immediate: true,// option das watcher sofort ausgeführt wird
+      // },
+      // totalVotes(newValue, oldValue) {
+      //   console.log(newValue);
+      //   console.log(oldValue);
+      // },
+    },
   },
 });
 
+//Globale Komponente
+app.component("SubmissionListItem", {
+  // Optionen
+  props: ["submission"],
+  methods: {
+    //Normale Funktion verwenden
+    // upvote(submissionId) {
+    //   // console.log(this)
+    //   const submission = this.submissions.find(
+    //     (submission) => submission.id === submissionId
+    //   );
+    //   submission.votes++;
+    // },
+    upvote() {
+     this.submission.votes++;
+    },
+  },
+  template: `
+  <div class="d-flex"> 
+    <div class="d-shrink-0">
+      <img v-bind:src="submission.img" alt="" />
+    </div>
+    <div class="flex-grow-1 ms-3">
+      <!-- <h5>
+        {{submission.title}}
+        <span
+          class="float-end text-primary"
+          v-on:click.right="upvote($event )"
+          style="cursor: pointer"
+        >
+          <i class="fa fa-chevron-up"></i>
+          <strong>{{submission.votes}}</strong>
+        </span>
+      </h5>-->
+        <h5>
+         <!-- {{index}} -->
+          {{submission.title}}
+        <span
+        class="float-end text-primary"
+        v-on:click="upvote()"
+        style="cursor: pointer"
+      >
+        <i class="fa fa-chevron-up"></i>
+        <strong>{{submission.votes}}</strong>
+      </span>
+      </h5>
+      <!-- <div>{{submission.desc}}</div> -->
+      <div v-html="submission.desc"></div>
+      <!-- <div v-pre>{{submission.desc}}</div> -->
+      <!-- Durch ein Object iterieren -->
+      <!-- <ul>
+        <li v-for="(value, key, index) in submission">
+         {{key}}: {{value}}, Index: {{index}} 
+        </li>
+      </ul> -->
+      <small class="text-muted">selected by {{submission.author}}</small>
+      <!-- <input type="text"> -->
+    </div>
+  </div>
+  `,
+});
 // Liefert eine Instanz zur Root-Component zurück
 const vm = app.mount("#app");
